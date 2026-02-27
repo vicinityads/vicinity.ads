32bd6df4cc751daa242e59416cc7f8bbc857e8ce
vicinityads <adsvicinity@gmail.com>
2026-02-27 16:31:56 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,12 @@
+your-repo/
+├── middleware.js      
+├── vercel.json        
+├── mybannerbaby.html
+├── ourstory.html
+├── ourapproach.html
+├── contact.html
+└── mobile/
+    ├── mybannerbaby.html
+    ├── ourstory.html
+    ├── ourapproach.html
+    └── contact.html
